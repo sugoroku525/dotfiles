5aaf7e6a86d41b420dcf182be056f9eb128661de
sugoroku525 <iwamasa816@keio.jp>
2024-04-17 16:58:13 +0900

    [ADD]install lazyvim and make configure files

--- dotfileslink.sh
+++ dotfileslink.sh
@@ -1,6 +1,25 @@
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/init.vim ~/init.vim
-
 # powerlevel10kのインストール
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+
+############ enhancdのインストール ############
+mkdir ~/Tools
+git clone git@github.com:b4b4r07/enhancd.git ~/Tools/enhancd
+echo 'source ~/Tools/enhancd/init.sh' >>~/.zshrc
+
+############ lazyvimのインストール ############
+# required
+mv ~/.config/nvim{,.bak}
+# optional but recommended
+mv ~/.local/share/nvim{,.bak}
+mv ~/.local/state/nvim{,.bak}
+mv ~/.cache/nvim{,.bak}
+# git clone nvim
+git clone https://github.com/LazyVim/starter ~/.config/nvim
+# remove git
+rm -rf ~/.config/nvim/.git
+
+############ 設定ファイルのリンク作成 ############
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/init.vim ~/init.vim
+ln -sf ~/dotfiles/nvim/lua/config/keymaps.lua ~/.config/nvim/lua/config/keymaps.lua
